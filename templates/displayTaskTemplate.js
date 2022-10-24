db0b327f3033055df89366a56d92d9b95df0ddb1
mukalawit <mgstn15@gmail.com>
2022-10-24 14:06:34 +0300

    chore: Add form on  display task template

--- templates/displayTaskTemplate.js
+++ templates/displayTaskTemplate.js
@@ -3,7 +3,8 @@ const layout = require("./layout");
 module.exports = (data) => {
   const renderedItems = data
     .map((item) => {
-      return `<li>Task name:${item.name} Task Description:${item.description} <a href='/update/${item.id}'>Edit</a></li>`;
+      return `<li>Task name:${item.name} Task Description:${item.description} <a href='/update/${item.id}'>Edit</a>
+      <form method="POST" action='/delete/${item.id}'><button>delete</button></form></li>`;
     })
     .join("");
 
