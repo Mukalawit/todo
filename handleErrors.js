b33ffa6cfd3c7aa42db0aae9248f37c39b3466c5
mukalawit <mgstn15@gmail.com>
2022-10-24 13:59:15 +0300

    chore: Delete unused code

--- handleErrors.js
+++
@@ -1,9 +0,0 @@
-const { validationResult } = require("express-validator");
-
-module.exports = (req, res, next) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    // console.log(errors);
-  }
-};
