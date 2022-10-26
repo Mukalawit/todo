e8112c31f433f826bd1dd56635a42858c090d81a
mukalawit <mgstn15@gmail.com>
2022-10-26 17:34:56 +0300

    chore:better presentation of list controls

--- templates/layout.js
+++ templates/layout.js
@@ -6,14 +6,17 @@ module.exports = ({ content }) => {
     <title>Task Manager</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css">
-
-    <!--
-    <link rel="stylesheet" href="../css/styles.css">
-    -->
+    <style>
+    .list-actions{
+      display:flex;
+      margin:5px;
+    }
+    </style>
     </head>
+    
     <body>
-    <div class="container is-widescreen">
-      <div class="notification is-primary">
+    <div class="hero">
+      <div class="notification is-info">
         <strong>TASK MANAGER</strong>
         <a href="/">Add a Task</a>
         <a href="/tasks">View Tasks</a>
