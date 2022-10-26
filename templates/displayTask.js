09c384a32a7decea756ed216e6e8326303101735
mukalawit <mgstn15@gmail.com>
2022-10-27 00:34:35 +0300

    feat: use cards for presentation

--- templates/displayTask.js
+++ templates/displayTask.js
@@ -3,21 +3,33 @@ const layout = require("./layout");
 module.exports = (data) => {
   const renderedItems = data
     .map((item) => {
-      return `<li ><strong>Task name:</strong>${item.name} <strong>Task Description:</strong>${item.description}
-      <div class="list-actions"><a href='/update/${item.id}' class="button is-primary">Edit</a>
-      <form method="POST" action='/delete/${item.id}'><button class="button is-danger">delete</button></form></div>
+      return `
+      <div class="column is-one-fifth">
       <div class="card">
-      <!--
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-      -->
-    </div>
-      </li>`;
+          <header class="card-header is-primary">
+            <p class="card-header-title">
+              Task: ${item.name}
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              ${item.description}
+            </div>
+          </div>
+          <footer class="card-footer">
+            <div class="card-footer-item"><a href="/update/${item.id}" class="button">Edit</a></div>
+            <div class="card-footer-item"><form method="POST" action='/delete/${item.id}'>
+            <button class="button">delete</button></form></div>
+          </footer>
+        </div>
+        </div>
+      `;
+
+
+
+
     })
     .join("");
 
-  return layout({ content: `<ol class="list-item">${renderedItems}</ol>` });
+  return layout({ content: `<div class="columns is-multiline">${renderedItems}</div>` });
 };
