afa40faabe3291d7363881bb4ee5be00642dc53e
mukalawit <mgstn15@gmail.com>
2022-10-26 15:00:06 +0300

    chore: add nav bar

--- templates/layout.js
+++ templates/layout.js
@@ -3,10 +3,25 @@ module.exports = ({ content }) => {
     <!DOCTYPE html>
     <html>
     <head>
-    <link rel='stylesheet' href='/css/main.css'>
+    <title>Task Manager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css">
+
+    <!--
+    <link rel="stylesheet" href="../css/styles.css">
+    -->
     </head>
     <body>
+    <div class="container is-widescreen">
+      <div class="notification is-primary">
+        <strong>TASK MANAGER</strong>
+        <a href="/">Add a Task</a>
+        <a href="/tasks">View Tasks</a>
+      </div>
+    </div>
+    <div class="content">
     ${content}
+    </div>
     </body>
     </html>
     `;
