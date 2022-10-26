fe30795fa6ce6eff8b156ae10a2e14acf1b88e0f
mukalawit <mgstn15@gmail.com>
2022-10-26 17:36:44 +0300

    chore: removed unsed code

--- templates/displayTask.js
+++ templates/displayTask.js
@@ -3,10 +3,21 @@ const layout = require("./layout");
 module.exports = (data) => {
   const renderedItems = data
     .map((item) => {
-      return `<li>Task name:${item.name} Task Description:${item.description} <a href='/update/${item.id}' class="button is-primary">Edit</a>
-      <form method="POST" action='/delete/${item.id}'><button class="button is-danger">delete</button></form></li>`;
+      return `<li ><strong>Task name:</strong>${item.name} <strong>Task Description:</strong>${item.description}
+      <div class="list-actions"><a href='/update/${item.id}' class="button is-primary">Edit</a>
+      <form method="POST" action='/delete/${item.id}'><button class="button is-danger">delete</button></form></div>
+      <div class="card">
+      <!--
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Save</a>
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+      -->
+    </div>
+      </li>`;
     })
     .join("");
 
-  return layout({ content: `<ul>${renderedItems}</ul>` });
+  return layout({ content: `<ol class="list-item">${renderedItems}</ol>` });
 };
